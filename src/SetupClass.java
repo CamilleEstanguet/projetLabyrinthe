cbbfdf6329780e9d8a0a98f43d7d52a868a6bb7b
Camille Estanguet <estanguetcamille@gmail.com>
2021-11-15 10:20:31 +0100

    Double labyrinthe jouable

--- src/SetupClass.java
+++ src/SetupClass.java
@@ -14,87 +14,71 @@ import org.newdawn.slick.tiled.TiledMap;
 
 public class SetupClass extends BasicGame {
 	
-	private Image knight = null;
-	private float x = 600, y = 400;
-	private float x2 = 100, y2 = 100;
-	Random rand = new Random();
-	
-	private Box box1, box2;
 	
 	private TiledMap map;
-
+	private int x,y, x2, y2;
 	public SetupClass(String title) {
 		super(title);
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void init(GameContainer container) throws SlickException {
-		knight = new Image("data/Jean-Pascal.png");
+	public void init(GameContainer container) throws SlickException //Implementation des elements presents au debut du jeu
+	{
+		map = new TiledMap("data/maps/mapDoubleLaby1.tmx");
 		
-		box1 = new Box(300, 300, 400, 350);
-        box2 = new Box(500, 500, 550, 550);
-        
-		map = new TiledMap("data/maps/map1.tmx");
-		
+		x = 2;
+		y = 2;
+		x2 = 17;
+		y2 = 2;
 	}
 	
-	public void update(GameContainer container, int delta) throws SlickException {
-		Input input = container.getInput();
-		
-		///=====================================DEPLACEMENTS=======================================================
-		if(input.isKeyDown(Input.KEY_D) || input.isKeyDown(Input.KEY_RIGHT)) {
-			x += 0.5 * delta;
-//			x2 += 0.5 * delta;
-		}
-		if(input.isKeyDown(Input.KEY_Z)|| input.isKeyDown(Input.KEY_UP)) {
-			y -= 0.5 * delta;
-//			y2 -= 0.5 * delta;
-		}
-		if(input.isKeyDown(Input.KEY_S) || input.isKeyDown(Input.KEY_DOWN)) {
-			y += 0.5 * delta;
-//			y2 += 0.5 * delta;
-		}
-		if(input.isKeyDown(Input.KEY_Q) || input.isKeyDown(Input.KEY_LEFT)) {
-			x -= 0.5 * delta; 
-//			x2 -= 0.5 * delta; 
-		}
-		
-		///================================HITBOX CIRCULAIRE, CHEVALIER==============================================
-		float d = (x-x2) * (x-x2) + (y-y2) * (y-y2);
-		float targetD = (15+15) * (15+15);
-		
-		if(d < targetD) {
-			x2 = rand.nextInt(750);
-			y2 = rand.nextInt(550);
-		}
-		
-		///====================================HITBOX BOITE, CUBE=====================================================
-		box2.setPos(x, y);
-        if (box1.collide(box2))
-        {
-            container.getGraphics().setColor(Color.red); //si collision, boite -> rouge
-        }
-        else
-        {
-            container.getGraphics().setColor(Color.white); //blanche par défaut
-        }
-        
+	public void update(GameContainer container, int delta) throws SlickException //Implementation des mechaniques de jeu
+	{
+		Input input = container.getInput();        
 		///==============================INTERACTIONS AVEC LA MAP===================================================
-        int objectLayer = map.getLayerIndex("objects");
+        int wallLayer = map.getLayerIndex("walls");
         
-        map.getTileId(0, 0, objectLayer);
+        map.getTileId(0, 0, wallLayer);
+        if(input.isKeyPressed(Input.KEY_D)) {
+        	if(map.getTileId(x+1, y, wallLayer)==0) {
+        		x++;
+        	}
+        	if(map.getTileId(x2+1, y2, wallLayer)==0) {
+        		x2++;
+        	}
+        }
+        if(input.isKeyPressed(Input.KEY_Q)) {
+        	if(map.getTileId(x-1, y, wallLayer)==0) {
+        		x--;
+        	}
+        	if(map.getTileId(x2-1, y2, wallLayer)==0) {
+        		x2--;
+        	}
+        }
+        if(input.isKeyPressed(Input.KEY_S)) {
+        	if(map.getTileId(x, y+1, wallLayer)==0) {
+        		y++;
+        	}
+        	if(map.getTileId(x2, y2+1, wallLayer)==0) {
+        		y2++;
+        	}
+        }
+        if(input.isKeyPressed(Input.KEY_Z)) {
+        	if(map.getTileId(x, y-1, wallLayer)==0) {
+        		y--;
+        	}
+        	if(map.getTileId(x2, y2-1, wallLayer)==0) {
+        		y2--;
+        	}
+        }
 	}
 	
-	public void render(GameContainer container, Graphics g) throws SlickException {
+	public void render(GameContainer container, Graphics g) throws SlickException //Implementation des graphismes du jeu
+	{
 		
-		map.render(0, 0);
-		knight.draw(x, y, 32, 32);
-		knight.draw(x2, y2, 32, 32);
-		
-		box1.render(container, g);
-        box2.render(container, g);
-		
-		
+		map.render(0, 0);	
+		g.fillRect(x*32, y*32, 32, 32);
+		g.fillRect(x2*32, y2*32, 32, 32);
 	}
 
 
